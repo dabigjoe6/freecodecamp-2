52cc65faf0f0e13cc38aea13f71d17ec344a0a55
Joseph Olabisi <josepholabisi6000@gmail.com>
2021-03-13 23:29:46 +0100

    Hello json

--- myApp.js
+++ myApp.js
@@ -7,6 +7,13 @@ app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+app.get('/json', (req, res) => {
+  res.json({
+    'message': 'Hello Json'
+  });
+});
+
 console.log('Hello World');
 
 
