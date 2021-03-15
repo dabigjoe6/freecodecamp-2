7f80eb8658a3f73922ddddb8e2aed829757331f7
Joseph Olabisi <josepholabisi6000@gmail.com>
2021-03-15 13:43:18 +0100

    Query param

--- myApp.js
+++ myApp.js
@@ -22,6 +22,14 @@ app.get('/now', (req, res, next) => {
   });
 });
 
+app.route('/name').get((req, res) => {
+  res.json({
+    name: `${req.query.first} ${req.query.last}`
+  })
+}).post((req, res) => {
+
+})
+
 app.get('/:word/echo', (req, res) => {
   res.json({
     echo: req.params.word
